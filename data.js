f97b5b7fcba4d016ffd89d0f83730f8b99c422be
ian0608 <ian.stainbrook@gmail.com>
2014-11-24 20:08:34 -0500

    Added Data API

---
+++ data.js
@@ -0,0 +1,499 @@
+var data = {
+	'drugs': {
+		'Enbrel': {
+			"Switzerland": 1017,
+			"England / UK": 1117,
+			"Spain": 1386,
+			"Netherlands": 1509,
+			"New Zealand": 1563,
+			"Canada": 1646,
+			"United States (25th Percentile)": 1946,
+			"United States": 2225,
+			"United States (95th Percentile)": 4006
+			},
+		'Gleevec': {
+			"Switzerland": 3633,
+			"England / UK": 2697,
+			"Spain": 3348,
+			"Netherlands": 3321,
+			"New Zealand": 989,
+			"Canada": 1141,
+			"United States (25th Percentile)": 5482,
+			"United States": 6214,
+			"United States (95th Percentile)": 11007
+			},
+		'Humira': {
+			"Switzerland": 881,
+			"England / UK": 1102,
+			"Spain": 1498,
+			"Netherlands": 1498,
+			"New Zealand": 1481,
+			"Canada": 1950,
+			"United States (25th Percentile)": 1913,
+			"United States": 2246,
+			"United States (95th Percentile)": 4089
+			},
+		'Copaxone': {
+			"Switzerland": 1357,
+			"England / UK": 862,
+			"Spain": 1191,
+			"Netherlands": 1190,
+			"New Zealand": 898,
+			"United States (25th Percentile)": 3875,
+			"United States": 3903,
+			"United States (95th Percentile)": 4018
+			},
+		'Gilenya': {
+			"Switzerland": 2499,
+			"England / UK": 2299,
+			"Spain": 2287,
+			"Netherlands": 2428,
+			"Canada": 2541,
+			"United States (25th Percentile)": 4169,
+			"United States": 5473,
+			"United States (95th Percentile)": 12792
+			},
+		'Celebrex': {
+			"Switzerland": 138,
+			"England / UK": 112,
+			"Spain": 164,
+			"Netherlands": 112,
+			"Canada": 51,
+			"United States (25th Percentile)": 139,
+			"United States": 225,
+			"United States (95th Percentile)": 461
+			},
+		'Cymbalta': {
+			"Switzerland": 76,
+			"England / UK": 46,
+			"Spain": 71,
+			"Netherlands": 52,
+			"Canada": 110,
+			"United States (25th Percentile)": 161,
+			"United States": 194,
+			"United States (95th Percentile)": 349
+			},
+		'Nexium': {
+			"Switzerland": 60,
+			"England / UK": 42,
+			"Spain": 58,
+			"Netherlands": 23,
+			"United States (25th Percentile)": 196,
+			"United States": 215,
+			"United States (95th Percentile)": 395
+			}
+		},
+	'diagnostics': {
+		'Angiogram': {
+			"Switzerland": 216,
+			"Spain": 290,
+			"Netherlands": 174,
+			"Australia": 741,
+			"Argentina": 818,
+			"United States (25th Percentile)": 196,
+			"United States": 907,
+			"United States (95th Percentile)": 2516
+			},
+		'CT Scan Abdomen': {
+			"Switzerland": 432,
+			"Spain": 94,
+			"Netherlands": 279,
+			"New Zealand": 731,
+			"Canada": 97,
+			"Australia": 500,
+			"Argentina": 128,
+			"United States (25th Percentile)": 299,
+			"United States": 896,
+			"United States (95th Percentile)": 2724
+			},
+		'MRI': {
+			"Switzerland": 138,
+			"Spain": 154,
+			"Netherlands": 461,
+			"New Zealand": 1005,
+			"Australia": 350,
+			"Argentina": 141,
+			"United States (25th Percentile)": 532,
+			"United States": 1145,
+			"United States (95th Percentile)": 2929
+			}
+		},
+	'procedures': {
+		'Hospital Cost Per Day': {
+			"Spain": 481,
+			"New Zealand": 2491,
+			"Australia": 1308,
+			"Argentina": 702,
+			"United States (25th Percentile)": 1447,
+			"United States": 4293,
+			"United States (95th Percentile)": 12726
+			},
+		'Appendectomy': {
+			"Switzerland": 9845,
+			"Spain": 2281,
+			"Netherlands": 4995,
+			"New Zealand": 6645,
+			"Australia": 5177,
+			"Argentina": 1723,
+			"United States (25th Percentile)": 8244,
+			"United States": 13910,
+			"United States (95th Percentile)": 29499
+			},
+		'Normal Delivery': {
+			"Switzerland": 8307,
+			"Spain": 2251,
+			"Netherlands": 2824,
+			"Australia": 6623,
+			"Argentina": 2237,
+			"United States (25th Percentile)": 7308,
+			"United States": 10002,
+			"United States (95th Percentile)": 17354
+			},
+		'C-Section': {
+			"Switzerland": 10681,
+			"Spain": 2844,
+			"Netherlands": 5492,
+			"Australia": 10263,
+			"Argentina": 2972,
+			"United States (25th Percentile)": 10641,
+			"United States": 15240,
+			"United States (95th Percentile)": 27446
+			},
+		'Cataract Surgery': {
+			"Spain": 2016,
+			"Netherlands": 1610,
+			"New Zealand": 3384,
+			"Australia": 3841,
+			"Argentina": 1038,
+			"United States (25th Percentile)": 2422,
+			"United States": 3762,
+			"United States (95th Percentile)": 8233
+			},
+		'Knee Replacement': {
+			"Switzerland": 24614,
+			"Spain": 8100,
+			"Netherlands": 12589,
+			"New Zealand": 20668,
+			"Australia": 21435,
+			"Argentina": 6015,
+			"United States (25th Percentile)": 16298,
+			"United States": 25398,
+			"United States (95th Percentile)": 51128
+			},
+		'Hip Replacement': {
+			"Switzerland": 19722,
+			"Spain": 8010,
+			"Netherlands": 11513,
+			"New Zealand": 19011,
+			"Australia": 26297,
+			"Argentina": 6862,
+			"United States (25th Percentile)": 16622,
+			"United States": 26489,
+			"United States (95th Percentile)": 53644
+			},
+		'Bypass Surgery': {
+			"Switzerland": 36509,
+			"Spain": 16247,
+			"Netherlands": 15742,
+			"New Zealand": 40368,
+			"Australia": 42130,
+			"Argentina": 16492,
+			"United States (25th Percentile)": 47982,
+			"United States": 75345,
+			"United States (95th Percentile)": 151886
+			},
+		'Angioplasty': {
+			"Switzerland": 10897,
+			"Spain": 10101,
+			"Netherlands": 5295,
+			"New Zealand": 16415,
+			"Australia": 8477,
+			"Argentina": 5246,
+			"United States (25th Percentile)": 16406,
+			"United States": 27907,
+			"United States (95th Percentile)": 61184
+			}
+		},
+	'equipment': {
+		'Hip Prosthesis': {
+			"Spain": 3177,
+			"New Zealand": 6723,
+			"Australia": 9982,
+			"United States (25th Percentile)": 6788,
+			"United States": 11806,
+			"United States (95th Percentile)": 25843
+			}
+		},
+	'statistics': {
+		'Physicians Per 1000 People': {
+			"Switzerland": 4.08,
+			"England / UK": 2.77,
+			"Spain": 3.96,
+			"Netherlands": 3.92,
+			"New Zealand": 2.74,
+			"Canada": 2.07,
+			"Australia": 3.85,
+			"Argentina": 3.16,
+			"United States": 2.42
+			},
+		'Life Expectancy At Birth': {
+			"Switzerland": 82.39,
+			"England / UK": 80.42,
+			"Spain": 81.47,
+			"Netherlands": 81.12,
+			"New Zealand": 80.93,
+			"Canada": 81.67,
+			"Australia": 82.07,
+			"Argentina": 77.51,
+			"United States": 79.56
+			},
+		'Health Expenditures as Percent of GDP': {
+			"Switzerland": 10.9,
+			"England / UK": 9.3,
+			"Spain": 9.6,
+			"Netherlands": 12,
+			"New Zealand": 10.1,
+			"Canada": 11.2,
+			"Australia": 9,
+			"Argentina": 8.1,
+			"United States": 17.9
+			},
+		'Population': {
+			"Switzerland": 8061516,
+			"England / UK": 63742977,
+			"Spain": 47737941,
+			"Netherlands": 16877351,
+			"New Zealand": 4401916,
+			"Canada": 34834841,
+			"Australia": 22507617,
+			"Argentina": 43024374,
+			"United States": 511434812
+			},
+		'Percentage Population Growth': {
+			"Switzerland": 0.78,
+			"England / UK": 0.54,
+			"Spain": 0.81,
+			"Netherlands": 0.42,
+			"New Zealand": 0.83,
+			"Canada": 0.76,
+			"Australia": 1.09,
+			"Argentina": 0.95,
+			"United States": 0.77
+			},
+		'GDP Per Capita': {
+			"Switzerland": 54800,
+			"England / UK": 37300,
+			"Spain": 30100,
+			"Netherlands": 43300,
+			"New Zealand": 30400,
+			"Canada": 43100,
+			"Australia": 43000,
+			"Argentina": 18600,
+			"United States": 52800
+			}
+		}
+	};
+	
+function getValue(categoryName, variableName, countryName)
+{
+	try {
+		var toReturn = data[categoryName][variableName][countryName];
+		if (toReturn == null)
+			return -1;
+		else
+			return toReturn;
+	}
+	catch(err) {
+		return -1;
+	}
+}
+
+function getValues(categoryName, variableName)
+{
+	try {
+		var toReturn = data[categoryName][variableName];
+		if (toReturn == null)
+			return -1;
+		else
+			return toReturn;
+	}
+	catch(err) {
+		return -1;
+	}
+}
+
+function getStatistics(countryName)
+{
+	var toReturn = {};
+	var statistics = data['statistics'];
+	
+	for (var stat in statistics) {
+		try {
+			var countryStat = statistics[stat][countryName];
+			if (countryStat == null)
+				toReturn[stat] = -1;
+			else
+				toReturn[stat] = countryStat;
+		}
+		catch (err) {
+			console.log("Error getting statistic " + stat + " for " + countryName);
+			toReturn[stat] = -1;
+		}
+	}
+	return toReturn;
+}
+
+function getArrayForStats(categoryName, variableName)
+{
+	var toReturn = [];
+	try {
+		var thisData = data[categoryName][variableName];
+		if (thisData == null)
+		{
+			toReturn.push(-1);
+			return toReturn;
+		}
+		else
+		{
+			for (var countryVal in thisData)
+			{
+				if (countryVal != "United States (25th Percentile)" && countryVal != "United States (95th Percentile)" && thisData[countryVal] != null)
+				{
+					toReturn.push(thisData[countryVal]);
+				}
+			}
+		}
+	}
+	catch(err) {
+		toReturn.push(-1);
+		return toReturn;
+	}
+	
+	return toReturn;
+}
+
+function getMean(categoryName, variableName)
+{
+	return ss.mean(getArrayForStats(categoryName, variableName));
+}
+
+function getMedian(categoryName, variableName)
+{
+	return ss.median(getArrayForStats(categoryName, variableName));
+}
+
+function getMax(categoryName, variableName)
+{
+	return ss.max(getArrayForStats(categoryName, variableName));
+}
+
+function getMin(categoryName, variableName)
+{
+	return ss.min(getArrayForStats(categoryName, variableName));
+}
+
+function get25Percent(categoryName, variableName)
+{
+	return ss.quantile(getArrayForStats(categoryName, variableName), .25);
+}
+
+function get75Percent(categoryName, variableName)
+{
+	return ss.quantile(getArrayForStats(categoryName, variableName), .75);
+}
+
+function getLinearRegressionFunction(xAxisCategory, xAxisVariable, yAxisCategory, yAxisVariable)
+{
+	var coords = [];
+	try {
+		var xAxisVar = data[xAxisCategory][xAxisVariable];
+		var yAxisVar = data[yAxisCategory][yAxisVariable];
+		if (xAxisVar == null || yAxisVar == null)
+		{
+			return -1;
+		}
+		else
+		{
+			for (var countryVal in xAxisVar)
+			{
+				if (xAxisVar[countryVal] != null && yAxisVar[countryVal] != null)
+				{
+					var pair = [];
+					pair.push(xAxisVar[countryVal]);
+					pair.push(yAxisVar[countryVal]);
+					coords.push(pair);
+				}
+			}
+			return ss.linear_regression().data(coords).line();
+		}
+	}
+	catch(err) {
+		return -1;
+	}
+}
+
+function getRankedValues(categoryName, variableName, desc)
+{
+	var array = [];
+	try {
+		var thisData = data[categoryName][variableName];
+		if (thisData == null)
+			return -1;
+		else
+		{
+			for (countryVal in thisData)
+			{
+				var subArray = [];
+				subArray.push(countryVal);
+				subArray.push(thisData[countryVal]);
+				array.push(subArray);
+			}
+			
+			if (desc)
+			{
+				array = array.sort(function (a, b) {
+					if (a[1] > b[1])
+						return -1;
+					else if (a[1] < b[1])
+						return 1;
+					else return 0;
+				});
+			}
+			else
+			{
+				array = array.sort(function (a, b) {
+					if (a[1] < b[1])
+						return -1;
+					else if (a[1] > b[1])
+						return 1;
+					else return 0;
+				});
+			}
+			return array;
+		}
+	}
+	catch(err) {
+		return -1;
+	}
+}
+
+
+$( document ).ready(function() {	//test the functions
+	console.log(getValue("statistics", "Population", "Argentina"));
+	console.log(getValues("statistics", "Population"));
+	console.log(getStatistics("Argentina"));
+	console.log(getMean("drugs", "Cymbalta"));
+	console.log(getMedian("drugs", "Cymbalta"));
+	console.log(getMax("drugs", "Cymbalta"));
+	console.log(getMin("drugs", "Cymbalta"));
+	console.log(get25Percent("drugs", "Cymbalta"));
+	console.log(get75Percent("drugs", "Cymbalta"));
+	var func = (getLinearRegressionFunction("procedures", "Hip Replacement", "statistics", "GDP Per Capita"));
+	console.log(func(8010));	//pass X value, get back Y value
+	console.log(func(26297));	//pass X value, get back Y value
+	console.log(getRankedValues("drugs", "Cymbalta", false));
+});
+
+
+
+
